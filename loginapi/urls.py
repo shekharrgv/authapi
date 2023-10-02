c79353e803b7faacec5ab26ed2474ffa2b1929a1
shekhar raghav <100549667+shekharrgv@users.noreply.github.com>
2023-10-03 00:52:56 +0530

    first commit

---
+++ loginapi/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from loginapi.views import SendPasswordResetEmailView, UserChangePasswordView, UserLoginView, UserProfileView, UserRegistrationView, UserPasswordResetView,LogoutView
+urlpatterns = [
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+    path('changepassword/', UserChangePasswordView.as_view(), name='changepassword'),
+    path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name='send-reset-password-email'),
+    path('reset-password/<uid>/<token>/', UserPasswordResetView.as_view(), name='reset-password'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
